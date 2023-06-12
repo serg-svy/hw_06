09bf4c6dc483198ca7f3a48060dafc00a13151a6
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-06-12 07:59:24 +0300

    update   unpack_path

--- sort.py
+++ sort.py
@@ -4,14 +4,18 @@ import string
 
 
 def normalize(name):
-
+    
+    
+    
     valid_chars = f"-_.() {string.ascii_letters}{string.digits}"
     normalized_name = ''.join(c if c in valid_chars else '_' for c in name)
     return normalized_name
 
 
-def process_file(file_path):
-
+def process_file(file_path, target_directory):
+    
+    
+    
     extension = file_path.rsplit('.', 1)[-1].lower()
     normalized_name = normalize(file_path)
     destination = ""
@@ -27,23 +31,21 @@ def process_file(file_path):
     elif extension in ['zip', 'gz', 'tar']:
         destination = 'archives'
         
-        unpack_path = os.path.join(destination, os.path.splitext(normalized_name)[0])
+        unpack_path = os.path.join(target_directory, destination, os.path.splitext(normalized_name)[0])
         shutil.unpack_archive(file_path, unpack_path)
 
     if destination:
-        os.makedirs(destination, exist_ok=True)
-        new_file_path = os.path.join(destination, normalized_name)
+        os.makedirs(os.path.join(target_directory, destination), exist_ok=True)
+        new_file_path = os.path.join(target_directory, destination, normalized_name)
         shutil.move(file_path, new_file_path)
 
 
 def process_directory(directory):
-
+    
     for root, dirs, files in os.walk(directory):
-        
-        dirs[:] = [d for d in dirs if d not in ['archives', 'video', 'audio', 'documents', 'images']]
         for file in files:
             file_path = os.path.join(root, file)
-            process_file(file_path)
+            process_file(file_path, directory)
 
 
 def main():
